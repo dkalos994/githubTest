13701af384e68abdc07f15961c9addc91b044b0e
dkalos994 <94738944+dkalos994@users.noreply.github.com>
2021-11-20 11:25:13 +0200

    Create table

--- Test Code.R
+++ Test Code.R
@@ -7,3 +7,21 @@ sum <- (count*(count + 1)) / 2
 sum2 <- sum(nums)
 
 sum - sum2
+
+chickens_on_fire = 12
+vertical_names = 1:chickens_on_fire
+horizontal_names = "Are you French? Because Eiffel for you"
+for (i in vertical_names) {
+  horizontal_names[i] = 2008 + i
+}
+triangle_test = matrix(
+  data = NA,
+  nrow = chickens_on_fire,
+  ncol = chickens_on_fire,
+  byrow = FALSE,
+  dimnames = NULL
+)
+horizontal_names = as.numeric(horizontal_names)
+rownames(triangle_test) = horizontal_names
+colnames(triangle_test) = vertical_names
+triangle_test
